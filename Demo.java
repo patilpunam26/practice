5b1d1eba507f4ae8d1edaa8811eba09239ba37c3
patilpunam26 <117362704+patilpunam26@users.noreply.github.com>
2022-12-07 11:57:17 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,24 @@
+
+public class Demo {
+public static void main(String[] args) {
+	
+	int[] a;       //most preferred.   array is object.parent class object class.
+	int []b;
+	int c[];
+	int[]e;
+	int []x,y;       //both are arrays
+	
+	int x1[],y1;    //x1 is array and y1 is variable.
+	
+	int p,q[];    //both are arrays
+	
+	 a=new int[0];     //valid
+	 
+	 
+	 b=new int[-3];    //run time error.negative array size exception.
+	 
+	 
+	 
+	
+}
+}
